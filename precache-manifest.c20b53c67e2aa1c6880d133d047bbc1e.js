6b736e5f8bc71e7522bd1a54071dbdcabca226f4
Dylan <dy.ashby@gmail.com>
2020-08-21 20:44:41 -0600

    Updates

---
+++ precache-manifest.c20b53c67e2aa1c6880d133d047bbc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94445a0df8ff5e2184143e22a5d2acaf",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bac29500172741e35850",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/static/css/main.32d387ae.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac29500172741e35850",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/static/js/main.2027cf38.chunk.js"
+  },
+  {
+    "revision": "583f0bdb5540dc487e4b",
+    "url": "/https//Cronvic.github.io/monsters-rolodex/static/js/runtime-main.c1f889b6.js"
+  }
+]);
